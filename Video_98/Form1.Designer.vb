3fdd4ba5b2cc95269c163bbb4f9a2ef9200bf576
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2022-02-01 12:34:40 -0500

    Finished

--- Video_98/Form1.Designer.vb
+++ Video_98/Form1.Designer.vb
@@ -22,10 +22,52 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnNewForm = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNewFormName = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnNewForm
+        '
+        Me.btnNewForm.Location = New System.Drawing.Point(12, 121)
+        Me.btnNewForm.Name = "btnNewForm"
+        Me.btnNewForm.Size = New System.Drawing.Size(404, 23)
+        Me.btnNewForm.TabIndex = 0
+        Me.btnNewForm.Text = "Show New Form"
+        Me.btnNewForm.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Name:"
+        '
+        'txtNewFormName
+        '
+        Me.txtNewFormName.Location = New System.Drawing.Point(12, 74)
+        Me.txtNewFormName.Name = "txtNewFormName"
+        Me.txtNewFormName.Size = New System.Drawing.Size(404, 23)
+        Me.txtNewFormName.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(428, 169)
+        Me.Controls.Add(Me.txtNewFormName)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnNewForm)
+        Me.Name = "Form1"
+        Me.Text = "Form Number 1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnNewForm As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNewFormName As TextBox
 End Class
--- Video_98/Form1.vb
+++ Video_98/Form1.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Form1
 
+    Private formNumber As Integer = 1
+    Private formName As String = "Default"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Form Number: " & formNumber & " Name: " & formName
+    End Sub
+
+    Private Sub btnNewForm_Click(sender As Object, e As EventArgs) Handles btnNewForm.Click
+        'dim newform as new form1
+        Dim newName As String = txtNewFormName.Text
+        'newform.formname = newname
+        'newform.formnumber = 2
+        'newform.showdialog()
+
+        Dim myNewForm As New Form2
+        myNewForm.Text = newName
+        myNewForm.Show()
+    End Sub
 End Class
